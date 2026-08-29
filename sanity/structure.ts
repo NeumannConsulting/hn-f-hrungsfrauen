5ed4cfd6abeb2bdaf6abbb8a72b7f582ee8f700d
Marek Neumann <123456789+marek@users.noreply.github.com>
2026-08-29 16:59:55 +0200

    Fix Sanity page structure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,20 +1,20 @@
 import type { StructureResolver } from 'sanity/structure';
 
 const editablePages = [
-  ['Startseite', 'page-startseite', 'startseite'],
-  ['Über uns', 'page-ueber-uns', 'ueber-uns'],
-  ['Netzwerk', 'page-netzwerk', 'netzwerk'],
-  ['Nachwuchsförderung', 'page-nachwuchs', 'nachwuchs'],
-  ['Veranstaltungen', 'page-veranstaltungen', 'veranstaltungen'],
-  ['Presse', 'page-presse', 'presse'],
-  ['Spenden', 'page-spenden', 'spenden'],
-  ['Kontakt', 'page-kontakt', 'kontakt'],
+  ['Startseite', 'page-startseite'],
+  ['Über uns', 'page-ueber-uns'],
+  ['Netzwerk', 'page-netzwerk'],
+  ['Nachwuchsförderung', 'page-nachwuchs'],
+  ['Veranstaltungen', 'page-veranstaltungen'],
+  ['Presse', 'page-presse'],
+  ['Spenden', 'page-spenden'],
+  ['Kontakt', 'page-kontakt'],
 ] as const;
 
 export const deskStructure: StructureResolver = (S) => S.list().title('Inhalte').items([
   S.listItem().title('Website-Einstellungen').child(S.document().schemaType('siteSettings').documentId('siteSettings')),
   S.divider(),
-  S.listItem().title('Seiteninhalte').child(S.list().title('Seiteninhalte').items(editablePages.map(([title, id, slug]) => S.listItem().title(title).child(S.document().schemaType('page').documentId(id).initialValue({ title, slug: { _type: 'slug', current: slug } }))))),
+  S.listItem().title('Seiteninhalte').child(S.list().title('Seiteninhalte').items(editablePages.map(([title, id]) => S.listItem().title(title).child(S.document().schemaType('page').documentId(id))))),
   S.documentTypeListItem('event').title('Veranstaltungen'),
   S.documentTypeListItem('person').title('Vorstand'),
   S.documentTypeListItem('pressArticle').title('Presseartikel'),
